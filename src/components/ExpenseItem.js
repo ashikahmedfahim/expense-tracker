6a95fe32a25c55585a3cbd663809693a05697bb6
ashikahmedfahim <41462787+ashikahmedfahim@users.noreply.github.com>
2021-11-27 21:39:38 +0600

    props added to component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,21 @@
 import React from "react";
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
-  const expenseDate = new Date();
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = "230";
+const ExpenseItem = (props) => {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString().substr(0, 10)}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">$ {expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">$ {props.amount}</div>
       </div>
     </div>
   );
